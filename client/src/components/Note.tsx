7465a3a82018fb589b5c1011bd9749cd64668a28
Brian Tam <briantam23@gmail.com>
2025-02-12 18:30:29 -0600

    Add disable functionality for add note button

--- client/src/components/Note.tsx
+++ client/src/components/Note.tsx
@@ -11,13 +11,19 @@ const Note = ({ notebook }) => {
 
     return (
         <div>
-            <h3 className="text-xl">Notes</h3>
+            <h3 className='text-xl'>Notes</h3>
             <textarea
                 onChange={(e) => setNote(e.target.value)}
                 value={note}
-                className="border-2 w-full p-2 dark:text-black"
+                className='border-2 w-full p-2 dark:text-black'
             />
-            <button onClick={addNote} className="p-2 mb-4 border dark:border-black bg-blue-500 text-white">Add Note</button>
+            <button
+                onClick={addNote}
+                className='p-2 mb-4 border dark:border-black bg-blue-500 text-white'
+                disabled={!note}
+            >
+                Add Note
+            </button>
             <ul className='list-disc list-inside text-gray-600 dark:text-gray-200 pb-8'>
                 {notebook.notes.map((note, idx) => (
                     <li key={idx}>{note}</li>
